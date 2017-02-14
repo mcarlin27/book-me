a41cdbf50195204aa6bb4e421e935e0831b6fbeb
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-02-13 18:42:18 -0800

    fix js issues by removing quotation marks.

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,10 @@ $(document).ready(function() {
     var timeInput = $("input#time").val();
     var reasonInput = $("input#reason").val();
 
-    $(".person").text("personInput");
-    $(".date").text("dateInput");
-    $(".time").text("timeInput");
-    $(".reason").text("reasonInput");
+    $(".person").text(personInput);
+    $(".date").text(dateInput);
+    $(".time").text(timeInput);
+    $(".reason").text(reasonInput);
 
     $("#confirmation").show();
 
